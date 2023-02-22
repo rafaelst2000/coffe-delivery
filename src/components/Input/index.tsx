8c6724e69368bf7289ba1dddd1967aabc353fd60
rafaelst2000 <rafaelst2000@gmail.com>
2023-02-22 19:34:25 -0300

    🐛 fix Input type

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,3 +1,4 @@
+import ReactInputMask from "react-input-mask";
 import { forwardRef, InputHTMLAttributes } from "react";
 import { InputStyleContainer, InputWrapper, InputStyled, RightText } from "./styles";
 
@@ -7,11 +8,11 @@ type InputProps = InputHTMLAttributes<HTMLInputElement> & {
   mask?: string
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>( ({ error, className, rightText, mask, ...props }, ref) => {
+export const Input = forwardRef<ReactInputMask, InputProps>( ({ error, className, rightText, mask, ...props }, ref) => {
   return (
     <InputWrapper className={className}>
       <InputStyleContainer hasError={!!error}>
-        <InputStyled mask={mask ? mask : null} onChange={props.onChange} value={props.value} {...props} ref={ref} />
+        <InputStyled mask={mask ? mask : ''} onChange={props.onChange} value={props.value} {...props} ref={ref} />
         {rightText && <RightText>{rightText}</RightText>}
       </InputStyleContainer>
       {error && <p>{error}</p>}
